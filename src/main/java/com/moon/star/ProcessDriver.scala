f0388c9b396813eef904091caae575688b1d6cba
liupei0210 <403664797@qq.com>
2018-03-29 11:02:53 +0800

    11

--- src/main/java/com/moon/star/ProcessDriver.scala
+++ src/main/java/com/moon/star/ProcessDriver.scala
@@ -2,8 +2,7 @@ package com.moon.star
 
 object ProcessDriver extends App{
   val process=List(Map("HdfsPath"->"hdfs://172.18.130.100/liupei/test/fiction.txt","nodeName"->"init","operation"->"","hasNext"->1),
-    Map("nodeName"->"flatMap",
-      "operation"->"_.split(\" \")","hasNext"->0))
+    Map("nodeName"->"flatMap", "operation"->"_.split(\" \")","hasNext"->0))
   val pe=new ProcessEngine
   pe.execute(process,null)
 }
